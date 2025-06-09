e332a8b09ffb4596f5fadd4534eceb5394a33f60
umbi301020 <umb.5grana@gmail.com>
2025-06-09 15:22:25 +0200

    Delete cat-ring-catering directory

--- cat-ring-catering/src/main/java/it/catering/catring/model/strategies/OrdinamentoStrategy.java
+++
@@ -1,9 +0,0 @@
-package it.catering.catring.model.strategies;
-
-import it.catering.catring.model.entities.Compito;
-import java.util.List;
-
-public interface OrdinamentoStrategy {
-    void ordina(List<Compito> compiti);
-    String getNome();
-}
