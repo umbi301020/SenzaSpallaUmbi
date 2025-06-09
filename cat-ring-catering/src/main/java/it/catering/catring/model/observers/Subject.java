234cc4de7d6620a718738d5cf0c6742210f0e8de
umbi301020 <umb.5grana@gmail.com>
2025-06-09 16:06:44 +0200

    Delete cat-ring-catering directory

--- cat-ring-catering/src/main/java/it/catering/catring/model/observers/Subject.java
+++
@@ -1,7 +0,0 @@
-package it.catering.catring.model.observers;
-
-public interface Subject<T> {
-    void addObserver(T observer);
-    void removeObserver(T observer);
-    void notifyObservers();
-}
